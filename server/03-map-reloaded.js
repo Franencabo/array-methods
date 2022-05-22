2e456a6075b1eb9d2fc729df6647813d3bda4b6f
Fran <franencabo@gmail.com>
2022-05-22 08:15:53 +0200

    Map reloaded

--- server/03-map-reloaded.js
+++ server/03-map-reloaded.js
@@ -22,5 +22,34 @@ const orders = [
 ];
 
 console.log('original', orders);
+
+
+// Si quiero sacar solo uno de los elementos
 const rta = orders.map(item => item.total);
 console.log('rta', rta);
+
+
+
+// Si quiero añadir un elemento pero SIN TRANSFORMAR el array original
+const rta2 = orders.map(item => {
+    item.tax = .19;
+    return item;
+})
+
+//Esto da la confusión por que modifica el valor que hay en memoria
+// solo ocurre con los objetos
+console.log('rta2', rta2);
+
+//Para evitar hacer copia nos tenemos que asegurar de generar un nuevo objeto
+// con el spread operator
+
+const rta3 = orders.map(item => {
+    item.tax = .19;
+    return {
+        ...item,
+        tax: .19
+    };
+});
+
+console.log('original', orders);
+console.log('rta3', rta3);
